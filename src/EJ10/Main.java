4d2d4322b41bf6980e698ddc14911711e0d2aac6
Ramón Guardia López <ramon.guardialopez@gmail.com>
2018-01-24 12:48:59 +0100

    EJ 11

--- src/EJ10/Main.java
+++ src/EJ10/Main.java
@@ -1,4 +1,34 @@
 package EJ10;
 
+import utils.JDBCUtils;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) {
+        /*Mostrar de todos los cursos el nombre
+        de la oferta educativa, la clave primaria del curso y el nombre del tutor.*/
+
+        ResultSet result;
+        JDBCUtils utils = JDBCUtils.getInstance();
+        String sqlProfesores = "SELECT oe.nombre, c.codOe, p.Nombre, p.Apellidos\n" +
+                "FROM OfertaEducativa oe\n" +
+                "RIGHT JOIN Curso c ON oe.codOe = c.codOe\n" +
+                "LEFT JOIN Profesor p ON c.codTutor = p.codProf;";
+        String cabecera = "------------------------------------------------------------------";
+        utils.startConnection();
+        try {
+            result = utils.executeSelect(sqlProfesores);
+            while (result.next()){
+                System.out.println(cabecera);
+                System.out.printf("Nombre de la oferta: %s %nCodigo de la oferta: %s %nNombre del tutor: %s %s%n",
+                        result.getString(1), result.getString(2),
+                        result.getString(3), result.getString(4));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        utils.closeConnection();
+    }
 }
--- src/EJ11/Main.java
+++ src/EJ11/Main.java
@@ -1,4 +1,32 @@
 package EJ11;
 
+import utils.JDBCUtils;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Main {
+    public static void main(String[] args) {
+        /*Mostrar cuándo se imparte una asignatura en un curso concreto. */
+        ResultSet result;
+        JDBCUtils utils = JDBCUtils.getInstance();
+        String sqlProfesores = "SELECT h.codOe, h.codCurso, a.nombre, th.dia, th.horaInicio, " +
+                "th.horaFin FROM Horario h  RIGHT JOIN Asignatura a ON a.codAsig = h.codAsig " +
+                "RIGHT JOIN TramoHorario th ON h.codTramo = th.codTramo WHERE a.codAsig = (?) " +
+                "AND h.codOe = (?);";
+        String cabecera = "------------------------------------------------------------------";
+        utils.startConnection();
+        try {
+            result = utils.executeSelect(sqlProfesores);
+            while (result.next()){
+                System.out.println(cabecera);
+                System.out.printf("Nombre de la oferta: %s %nCodigo de la oferta: %s %nNombre del tutor: %s %s%n",
+                        result.getString(1), result.getString(2),
+                        result.getString(3), result.getString(4));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        utils.closeConnection();
+    }
 }
