3ffd4d4594c8a59867bae4d9e45aee389aeddcdd
Ramón Guardia López <ramon.guardialopez@gmail.com>
2018-01-25 20:57:34 +0100

    v 0.12 Intento solucionar 13

--- src/EJ13/Main.java
+++ src/EJ13/Main.java
@@ -9,8 +9,9 @@ public class Main {
     public static void main(String[] args) {
        /* Mostrar el horario de un curso en modo tabla. Añadir un asterisco en aquellos tramos
             horarios donde haya desdoble.*/
-       //TODO mirar esto
+        //TODO mirar esto
         ResultSet result;
+        boolean entra = false;
         int contador = 0;
         String codOe = "DAM";
         String codCurso = "1A";
@@ -18,26 +19,26 @@ public class Main {
         String sqlHorario = String.format("SELECT th.horaInicio, th.horaFin, h.codAsig, h.codTramo, th.dia\n" +
                 "FROM Horario h JOIN TramoHorario th on h.codTramo = th.codTramo\n" +
                 "WHERE h.codCurso = '%s' AND h.codOe = '%s'\n" +
-                "ORDER BY th.horaInicio, th.dia, h.codAsig DESC;",codCurso,codOe);
+                "ORDER BY th.horaInicio, th.dia, h.codAsig DESC;", codCurso, codOe);
         utils.startConnection();
         try {
             result = utils.executeSelect(sqlHorario);
             System.out.printf("%-20s %11s  %11s  %11s  %11s  %11s%n",
-                    "Horas","Lunes", "Martes", "Miércoles","Jueves", "Viernes");
-
-            while (result.next()){
-                if(contador == 0){
-                    System.out.printf("%s / %s",result.getString(1),result.getString(2)," ");
+                    "Horas", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes");
+            while (result.next()) {
+                if (contador == 0) {
+                    System.out.printf("%s / %s", result.getString(1), result.getString(2), " ");
                 }
-                if(contador < 5){
-                    /*if(result.getString(3).startsWith("@")){
+                if (contador < 5) {
+                    if (result.getString(3).startsWith("@")) {
                         result.next();
-                        System.out.printf("%13s",result.getString(3).concat("*"));
-                    }else{*/
-                        System.out.printf("%13s",result.getString(3));
-                    //}
-                    contador ++;
-                }else{
+                        System.out.printf("%13s", result.getString(3).concat("*"));
+                        contador++;
+                    } else {
+                        System.out.printf("%13s", result.getString(3));
+                        contador++;
+                    }
+                } else {
                     System.out.println();
                     contador = 0;
                 }
---
+++ src/EJ14/Main.java
@@ -0,0 +1,8 @@
+package EJ14;
+
+public class Main {
+    public static void main(String[] args) {
+     /* Mostrar dónde se encuentra un profesor en un tramo horario concreto. */
+
+    }
+}
